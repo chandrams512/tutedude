473417c41dd533ea08eb612085319709bea91210
Manish chandra <Manish.chandra.sh@gmail.com>
2025-07-19 00:51:07 +0530

    Add files via upload
    Assignment 1- Basic Python Concepts
    Task1

---
+++ Task1.py
@@ -0,0 +1,18 @@
+# Task 1
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+Addition = num1+num2
+Subtraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+print("Addition:" + str(Addition))
+print("Subtraction:" + str(Subtraction))
+print("Multiplication:" + str(Multiplication))
+print("Division:" + str(Division))
+
+# Task 2
+
+
+
