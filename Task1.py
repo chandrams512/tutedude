a92d51ac626038c8a56a765e7ea690c4e54ec740
Manish chandra <Manish.chandra.sh@gmail.com>
2025-08-13 19:02:30 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -1,18 +1,13 @@
 # Task 1
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num = input("Enter a number: ")
 
-Addition = num1+num2
-Subtraction = num1 - num2
-Multiplication = num1 * num2
-Division = num1 / num2
-print("Addition:" + str(Addition))
-print("Subtraction:" + str(Subtraction))
-print("Multiplication:" + str(Multiplication))
-print("Division:" + str(Division))
+if(int(num) % 2 == 0):
+    print(num + " is an even number")
+else:
+    print(num + " is an odd number")
 
-# Task 2
+
 
 
 
