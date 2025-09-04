e3a36e1031e0fcb6c50aa7526b7fdb268ffb1e2a
Manish chandra <Manish.chandra.sh@gmail.com>
2025-09-04 15:34:49 +0530

    Task2.py
    1.   Creates a list of numbers from 1 to 10.
    2.   Extracts the first five elements from the list.
    3.   Reverses these extracted elements.
    4.   Prints both the extracted list and the reversed list

--- Task2.py
+++ Task2.py
@@ -1,16 +1,7 @@
 # Task 2
-# Step 1: Take user input and write to the file (overwrite mode)
-user_text = input("Enter text to write to the file: ")
-with open("output.txt", "w") as file:
-    file.write(user_text + "\n")
-
-# Step 2: Take more input and append to the file
-user_text2 = input("Enter additional text to append: ")
-with open("output.txt", "a") as file:
-    file.write(user_text2 + "\n")
-
-# Step 3: Read and display final content
-with open("output.txt", "r") as file:
-    content = file.read()
-    print("\nFinal file content:\n")
-    print(content)
+original_list = [x for x in range(1,11)]
+print(original_list)
+extracted_first_five = original_list[:5]
+print(extracted_first_five)
+extracted_first_five.reverse()
+print(extracted_first_five)
