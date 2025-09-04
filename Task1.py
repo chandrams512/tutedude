9f9956198f1f1a6ee659d491f24c16f952a9a314
Manish chandra <Manish.chandra.sh@gmail.com>
2025-09-04 15:08:38 +0530

    Task1.py
    Write a Python program that:
    1.   Creates a dictionary where student names are keys and their marks are values.
    2.   Asks the user to input a student's name.
    3.   Retrieves and displays the corresponding marks.
    4.   If the student’s name is not found, display an appropriate message.

--- Task1.py
+++ Task1.py
@@ -1,10 +1,18 @@
 # Task 1
 
-try:
-    with open('sample.txt', 'r') as file:
-        for line in file:
-            print(line.strip())
-except FileNotFoundError:
-    print("Error: The file sample.txt was not found")
+students = {
+    "Manish" : 90,
+    "Aarvi" : 95,
+    "Manny" : 76,
+    "Ravi" : 65,
+    "Alice" : 85
+}
+
+student = input("Enter the student name: ")
+
+if student in students:
+    print(f"{student}'s marks: {students[student]}")
+else:
+    print("Student not found.")
 
 
